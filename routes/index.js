8f1320f05c0fa99af82e00562677e34e4c4da860
Kunalsahuji <ksahu0103@gmail.com>
2024-08-01 18:48:36 +0530

    Worked on styling

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
+const  isLoggedIn  = require('../utils/auth')
 const path = require('path');
 const fs = require('fs');
-// const upload = require('../utils/multer').single("profilePic");
 const upload = require('../utils/multer')
 const User = require('../model/userModel')
 const Post = require('../model/postModel')
@@ -12,37 +11,31 @@ const LocalStrategy = require('passport-local');
 const sendmail = require("../utils/mail")
 passport.use(new LocalStrategy(User.authenticate()))
 
-
-/* GET home page. */
-router.get('/', isLoggedIn, (req, res, next) => {
+// homepage
+router.get('/', (req, res, next) => {
   res.render('index', { user: req.user });
 });
 
+// get login page
 router.get('/login', function (req, res, next) {
   res.render('login', { user: req.user });
 });
 
+// post login page
 router.post('/login-user',
   passport.authenticate("local", {
-    successRedirect: "/",
+    successRedirect: "/profile",
     failureRedirect: "/login"
   }),
   function (req, res, next) {
   });
 
-function isLoggedIn(req, res, next) {
-  if (req.isAuthenticated()) {
-    next();
-  }
-  else {
-    res.redirect('/login')
-  }
-}
-
+// get register page
 router.get('/register', function (req, res, next) {
   res.render('register', { user: req.user });
 });
 
+// post register page
 router.post('/register-user', async function (req, res, next) {
   try {
     const { name, username, email, password } = req.body;
@@ -50,56 +43,52 @@ router.post('/register-user', async function (req, res, next) {
       { name, username, email }, password
     )
     res.redirect('/login',);
-
-    // const newUser = new User(req.body)
-    // await newUser.save()
-    // res.redirect('/login',);
   } catch (error) {
     console.log(error)
     res.send(error.message)
   }
 });
 
+//get forget-user
 router.get('/forget-user', function (req, res, next) {
   res.render('fotget-user',);
 });
 
+// get about page
 router.get('/about', function (req, res, next) {
   res.render('about', { user: req.user });
 });
 
-
-
+// get logout-user
 router.get('/logout-user/:id', isLoggedIn, function (req, res, next) {
   req.logout(() => {
     res.redirect('/login')
   })
 });
 
+// get update-user/:id
 router.get('/update-user/:id', isLoggedIn, function (req, res, next) {
   res.render('update-user', { user: req.user });
 });
+
+// post update-user/:id
 router.post('/update-user/:id', isLoggedIn, async (req, res, next) => {
   try {
     const id = req.params.id
     await User.findByIdAndUpdate(id, req.body)
     console.log(`Updated User: ${req.user}`)
-
-    // console.log(req.user)
-    // res.render('update-user', { user: req.user });
-    // console.log("User Updated!")
     res.redirect('/profile')
   } catch (error) {
     res.send(error)
   }
 });
 
-
-
+//get reset-password/:id
 router.get('/reset-password/:id', isLoggedIn, function (req, res, next) {
   res.render('reset-password', { user: req.user });
 });
 
+//post reset-password/:id
 router.post('/reset-password/:id', isLoggedIn, async function (req, res, next) {
   try {
     await req.user.changePassword(
@@ -114,21 +103,19 @@ router.post('/reset-password/:id', isLoggedIn, async function (req, res, next) {
   }
 });
 
-
+//get forget-email/:id
 router.get('/forget-email/', function (req, res, next) {
   res.render('forget-email', { user: req.user });
 });
 
+//post forget-email/:id
 router.post('/forget-email/', async (req, res, next) => {
   try {
     const user = await User.findOne({ email: req.body.forgetEmail })
-    // res.redirect(`/forget-password/${user._id}`)
     if (user) {
-      // const url = `${req.protocol}://${req.get("host")}${req.originalUrl}`;
       const url = `${req.protocol}://${req.get("host")}/forget-password/${user._id}`
       console.log(`url = ${url}`)
       sendmail(res, user, url)
-      // res.redirect(`/forget-password/${user._id}`)
     }
     else {
       res.redirect("/forget-email")
@@ -139,22 +126,22 @@ router.post('/forget-email/', async (req, res, next) => {
   }
 });
 
-
+//get forget-password/:id
 router.get('/forget-password/:id', function (req, res, next) {
   res.render('forget-password', { user: req.user, id: req.params.id });
 });
 
+//post forget-password/:id
 router.post('/forget-password/:id', async function (req, res, next) {
   try {
     const user = await User.findById(req.params.id)
-    // await user.setPassword(req.body.newPassword)
     if (user.resetPasswordToken === 1) {
       await user.setPassword(req.body.newPassword);
       user.resetPasswordToken = 0;
       await user.save();
       res.redirect('/login')
     } else {
-      res.send("Link Expired Try Again!");
+      res.send("Link Expired. Try Again!");
     }
   } catch (error) {
     console.log(error.message)
@@ -162,6 +149,7 @@ router.post('/forget-password/:id', async function (req, res, next) {
   }
 });
 
+//get delete-user/:id
 router.get('/delete-user/:id', isLoggedIn, async (req, res, next) => {
   try {
     const id = req.params.id
@@ -181,13 +169,13 @@ router.get('/delete-user/:id', isLoggedIn, async (req, res, next) => {
   }
 });
 
+//get profile page
 router.get('/profile', isLoggedIn, async (req, res, next) => {
-  // const user = await User.findById(req)
-  // console.log(req.user)
-  // res.render('profile', { user: req.user });
   try {
     const posts = await Post.find().populate("user")
-    console.log(`Profile:-- user: ${req.user}\npost: ${posts}`)
+    posts.forEach((elem) => {
+      console.log(`ye mera h ${elem.user.username}`)
+    })
     res.render('profile', { user: req.user, posts });
   } catch (error) {
     console.log(error.message)
@@ -195,8 +183,8 @@ router.get('/profile', isLoggedIn, async (req, res, next) => {
   }
 });
 
+// get timeline page
 router.get('/timeline', isLoggedIn, async (req, res, next) => {
-
   try {
     res.render('timeline', { user: await req.user.populate("posts") });
   } catch (error) {
@@ -204,6 +192,8 @@ router.get('/timeline', isLoggedIn, async (req, res, next) => {
     res.send(error)
   }
 });
+
+//get delete-post/:id
 router.get('/delete-post/:id', isLoggedIn, async (req, res, next) => {
   try {
     const deletepost = await Post.findByIdAndDelete(req.params.id)
@@ -214,6 +204,8 @@ router.get('/delete-post/:id', isLoggedIn, async (req, res, next) => {
     res.send(error)
   }
 })
+
+//get update-post/:id
 router.get('/update-post/:id', isLoggedIn, async (req, res, next) => {
   try {
     const post = await Post.findById(req.params.id)
@@ -224,6 +216,7 @@ router.get('/update-post/:id', isLoggedIn, async (req, res, next) => {
   }
 })
 
+//post update-post/:id
 router.post('/update-post/:pid', isLoggedIn, async (req, res, next) => {
   const post = await Post.findByIdAndUpdate(req.params.pid, req.body)
   // res.redirect(`/update-post/${req.params.pid}`)
@@ -231,6 +224,7 @@ router.post('/update-post/:pid', isLoggedIn, async (req, res, next) => {
 
 })
 
+//post post-image/:id
 router.post('/post-image/:pid', isLoggedIn, upload.single("media"), async (req, res, next) => {
   try {
     const post = await Post.findById(req.params.pid)
@@ -238,16 +232,18 @@ router.post('/post-image/:pid', isLoggedIn, upload.single("media"), async (req, 
     post.media = req.file.filename
     await post.save()
     res.redirect(`/update-post/${req.params.pid}`)
-    // res.redirect(`/timeline${req.params.pid}`)
   } catch (error) {
     console.log(error)
     res.send(error)
   }
 })
+
+// get create-post
 router.get('/create-post/', isLoggedIn, (req, res, next) => {
   res.render('create-post', { user: req.user })
 })
 
+//post create-post
 router.post('/create-post/', isLoggedIn, upload.single("media"), async (req, res, next) => {
   try {
     const newPost = new Post({
@@ -266,6 +262,8 @@ router.post('/create-post/', isLoggedIn, upload.single("media"), async (req, res
     res.send(error)
   }
 })
+
+// post image/:id
 router.post("/image/:id", isLoggedIn, upload.single("profilePic"), async (req, res, next) => {
   try {
     if (req.user.profilePic !== "Default.png") {
@@ -280,6 +278,7 @@ router.post("/image/:id", isLoggedIn, upload.single("profilePic"), async (req, r
   }
 })
 
+// get like/:id
 router.get('/like/:postid', isLoggedIn, async (req, res, next) => {
   try {
     const post = await Post.findById(req.params.postid)
@@ -297,6 +296,7 @@ router.get('/like/:postid', isLoggedIn, async (req, res, next) => {
   }
 })
 
+// get likes/:id
 router.get('/likes/:postid', isLoggedIn, async (req, res, next) => {
   try {
     const post = await Post.findById(req.params.postid)
